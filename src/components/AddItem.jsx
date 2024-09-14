c515d2858ba9d0f252e36bba74911e59aa34c0a6
tarunrajput <tr393064@gmail.com>
2024-09-14 11:18:34 +0530

    first commit

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const AddItem = ({ addItem }) => {
+  const [newItem, setNewItem] = useState('');
+
+  const handleAdd = () => {
+    addItem(newItem);
+    setNewItem(''); // Clear the input
+  };
+
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        placeholder="Add new item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
+
+export default AddItem;
